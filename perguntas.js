11d60e15ff8ee77ceb1b48ee795336893fb184ed
laritsilva <tanajura.larissa@escola.pr.gov.br>
2025-08-18 13:09:07 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,34 +1,34 @@
-criarCartao(
+criaCartao(
     'Biologia',
     'Qual é o maior órgão do corpo humano',
     'A  pele.'
 )
 
-criarCartao(
+criaCartao(
     'Matemática',
     'Qual é a fórmula da área de um triângulo?',
     'A área de um triângulo é dada por (base x altura) / 2.'
 )
 
-criarCartao(
+criaCartao(
     'História',
     'Quem foi o primeiro presidente do Brasil?',
     'O primeiro presidente do Brasil foi Deodoro da Fonseca.'
 )
 
-criarCartao(
+criaCartao(
     'Geografia',
     'Qual é o maior continente do mundo?',
     'O maior continente do mundo é a Ásia.'
 )
 
-criarCartao(
+criaCartao(
     'Química',
     'Qual é o elemento químico com símbolo O?',
     'O elemento químico com símbolo O é o Oxigênio.'
 )
 
-criarCartao(
+criaCartao(
     'Física',
     'O que é a força da gravidade?',
     'A força da gravidade é a atração que um corpo exerce sobre outro devido à sua massa.'
